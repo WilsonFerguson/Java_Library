9eb2b521a9c47a861b36bf2c358a98b14d18f139
BuildTools <unconfigured@null.spigotmc.org>
2022-10-01 19:42:18 -0700

    Added javadocs to the Window class.

--- Window.java
+++ Window.java
@@ -39,6 +39,14 @@ public class Window {
     private double sleepTime = 0;
     private double nextGameTick = 0;
 
+    /**
+     * Creates a new window given {@code int} width, {@code int} height, and
+     * {@code String} title.
+     * 
+     * @param width
+     * @param height
+     * @param title
+     */
     public Window(int width, int height, String title) {
         this.width = width;
         this.height = height;
@@ -51,11 +59,17 @@ public class Window {
         deltaTime = 0;
     }
 
+    /**
+     * Creates a new window given {@code int} width and {@code int} height.
+     * 
+     * @param width
+     * @param height
+     */
     public Window(int width, int height) {
         this(width, height, "");
     }
 
-    public void createWindow() {
+    private void createWindow() {
         window = new JFrame(title);
         window.setSize(width, height);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -103,7 +117,7 @@ public class Window {
         });
     }
 
-    public void callMethod(String methodName) {
+    private void callMethod(String methodName) {
         try {
             Class<?> c = Class.forName("ClickableObject");
             Method method = c.getDeclaredMethod(methodName);
@@ -117,43 +131,95 @@ public class Window {
         }
     }
 
+    /**
+     * Returns the {@code JFrame} window.
+     * 
+     * @return JFrame
+     */
     public JFrame getWindow() {
         return window;
     }
 
+    /**
+     * Returns the {@code Graphics} graphics of the window.
+     * 
+     * @return Graphics
+     */
     public Graphics getGraphics() {
         return window.getGraphics();
     }
 
+    /**
+     * Returns the {@code int} width of the window.
+     * 
+     * @return int
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * Returns the {@code int} height of the window.
+     * 
+     * @return int
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * Returns the {@code String} title of the window.
+     * 
+     * @return String
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the window to full screen.
+     */
     public void setFullscreen() {
         window.setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
 
+    /**
+     * Sets the background given a {@code Color} color.
+     * 
+     * @param color
+     */
     public void background(Color color) {
         window.getContentPane().setBackground(color);
         backgroundColor = color;
     }
 
+    /**
+     * Sets the background given an {@code int} red, {@code int} green, and
+     * {@code int} blue.
+     * 
+     * @param red
+     * @param green
+     * @param blue
+     */
     public void background(int r, int g, int b) {
         background(new Color(r, g, b));
     }
 
+    /**
+     * Sets the background given an {@code int} gray.
+     * 
+     * @param gray
+     */
     public void background(int gray) {
         background(gray, gray, gray);
     }
 
+    /**
+     * Sets the target frame rate given a {@code double} target frame rate. If the
+     * target frame rate is -1, the frame rate is set to 1000.
+     * 
+     * @param targetFrameRate
+     */
     public void setTargetFrameRate(double targetFrameRate) {
         if (targetFrameRate > 0)
             this.targetFrameRate = targetFrameRate;
@@ -161,6 +227,11 @@ public class Window {
             this.targetFrameRate = 1000;
     }
 
+    /**
+     * Returns the {@code double} target frame rate.
+     * 
+     * @return double
+     */
     public double getTargetFrameRate() {
         return targetFrameRate;
     }
@@ -196,7 +267,7 @@ public class Window {
         };
     }
 
-    public void draw() {
+    private void draw() {
         JPanel panel = drawPanels();
         window.add(panel);
 
@@ -228,9 +299,12 @@ public class Window {
 
     }
 
+    /**
+     * Displays the frame rate of the window in the top right corner.
+     */
     public void displayFramerate() {
         double fps = getFrameRate();
-        Rectangle rectangle = new Rectangle(width - 100, 0, 100, 20);
+        Rectangle rectangle = new Rectangle(width - 700, 0, 100, 20);
         rectangle.color(0, 0, 0);
         Text text = new Text(Helper.roundString(fps, 0), width - 700, 10);
         text.color(255, 255, 255);
@@ -239,7 +313,7 @@ public class Window {
         add(text);
     }
 
-    public void handleInputs() {
+    private void handleInputs() {
         try {
             mouseX = window.getMousePosition().x;
             mouseY = window.getMousePosition().y;
@@ -266,7 +340,7 @@ public class Window {
 
     }
 
-    public void controlFrameRate() {
+    private void controlFrameRate() {
         int skipTicks = 1000 / (int) targetFrameRate;
 
         nextGameTick += skipTicks;
@@ -275,6 +349,9 @@ public class Window {
             Helper.wait(sleepTime);
     }
 
+    /**
+     * Updates the window. Must be called at the start of your loop.
+     */
     public void update() {
         if (targetFrameRate < 1000)
             controlFrameRate();
@@ -289,62 +366,138 @@ public class Window {
         clickableObjects.clear();
     }
 
+    /**
+     * Adds a {@code JPanel} jpanel to the window.
+     * 
+     * @param panel
+     */
     public void add(JPanel panel) {
         panels.add(panel);
     }
 
+    /**
+     * Adds a {@code ClickableObject} clickable object to the window.
+     * 
+     * @param clickableObject
+     */
     public void add(ClickableObject clickableObject) {
         clickableObjects.add(clickableObject);
     }
 
+    /**
+     * Closes the window.
+     */
     public void close() {
         window.dispose();
     }
 
+    /**
+     * Returns the {@code Point} mouse position.
+     * 
+     * @return Point
+     */
     public Point getMouse() {
         return new Point(mouseX, mouseY);
     }
 
+    /**
+     * Returns {@code boolean} if the mouse is pressed.
+     * 
+     * @return boolean
+     */
     public boolean mousePressed() {
         return mousePressed;
     }
 
+    /**
+     * Returns {@code boolean} if the mouse is released.
+     * 
+     * @return boolean
+     */
     public boolean mouseReleased() {
         return !mousePressed;
     }
 
+    /**
+     * Returns {@code boolean} if a key is pressed.
+     * 
+     * @return boolean
+     */
     public boolean keyPressed() {
         return !key.equals("");
     }
 
-    public boolean keyJustReleased() {
-        return keyJustReleased;
-    }
-
+    /**
+     * Returns {@code boolean} if a key was just pressed.
+     * 
+     * @return boolean
+     */
     public boolean keyJustPressed() {
         return keyJustPressed;
     }
 
+    /**
+     * Returns {@code boolean} if a key was just released.
+     * 
+     * @return boolean
+     */
+    public boolean keyJustReleased() {
+        return keyJustReleased;
+    }
+
+    /**
+     * Returns {@code String} key that is currently pressed. If no keys are pressed,
+     * returns {@code ""}.
+     * 
+     * @return String
+     */
     public String key() {
         return key;
     }
 
+    /**
+     * Returns {@code int} key code that is currently pressed. If no keys are
+     * pressed, returns {@code -1}.
+     * 
+     * @return int
+     */
     public int keyCode() {
         return keyCode;
     }
 
+    /**
+     * Returns how long the window has been open in {@double} milliseconds.
+     * 
+     * @return double
+     */
     public double getTimeMillis() {
         return upTime - startTime;
     }
 
+    /**
+     * Returns how long the window has been open in {@double} seconds.
+     * 
+     * @return double
+     */
     public double getTimeSeconds() {
         return (upTime / 1000.0) - (startTime / 1000.0);
     }
 
+    /**
+     * Returns the delta time in {@double} milliseconds.
+     * 
+     * @return double
+     */
     public double getDeltaTime() {
         return deltaTime;
     }
 
+    /**
+     * Returns the frame rate of the window in {@double} frames per second.
+     * Framerate is capped at 1000 frames per second.
+     * 
+     * @return double
+     */
     public double getFrameRate() {
         return Math.min(1000.0 / deltaTime, 1000); // Cap at 1000
     }
